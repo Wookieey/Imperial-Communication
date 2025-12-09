647abc3f2db6b90abb8b378bafdf98fcee53e1ca
Mr.boblob <114491117+Wookieey@users.noreply.github.com>
2025-12-09 13:10:40 -0600

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const authenticateUser = async (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.status(401).json({ message: "No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch (error) {
+        res.status(403).json({ message: "Invalid token." });
+    }
+};
