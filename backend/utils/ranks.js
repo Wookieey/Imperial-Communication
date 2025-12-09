27db306a99a6b4ff1a0637a8b54d5c2f985b7a5a
Mr.boblob <114491117+Wookieey@users.noreply.github.com>
2025-12-09 13:43:12 -0600

    Create ranks.js

---
+++ backend/utils/ranks.js
@@ -0,0 +1,13 @@
+export default {
+    "Stormtrooper": 1,
+    "Lieutenant": 2,
+    "Captain": 3,
+    "Commander": 4,
+    "Major": 5,
+    "Colonel": 6,
+    "General": 7,
+    "Admiral": 8,
+    "Moff": 9,
+    "Grand Moff": 10,
+    "Emperor": 11
+};
