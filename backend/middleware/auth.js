62c042885451bfcf6596b208f5f5558cc4603230
Mr.boblob <114491117+Wookieey@users.noreply.github.com>
2025-12-10 12:24:29 -0600

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,15 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-export const authenticateUser = async (req, res, next) => {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
+export default async function (req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ msg: "Missing token" });
 
-    if (!token) {
-        return res.status(401).json({ message: "No token provided." });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
-    } catch (error) {
-        res.status(403).json({ message: "Invalid token." });
-    }
-};
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch {
+    return res.status(401).json({ msg: "Invalid token" });
+  }
+}
