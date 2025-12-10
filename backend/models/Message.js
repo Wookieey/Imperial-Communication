43a7f1284fe2f25d045ce0841ebca3faa28cc28f
Mr.boblob <114491117+Wookieey@users.noreply.github.com>
2025-12-10 12:14:03 -0600

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,33 +1,10 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-    roomId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Room",
-        required: true
-    },
+const MessageSchema = new mongoose.Schema({
+  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  mentions: [String],
+}, { timestamps: true });
 
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-
-    content: {
-        type: String,
-        required: true
-    },
-
-    mentions: {
-        users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-        ranksNotified: [String],
-        everyone: { type: Boolean, default: false }
-    },
-
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});
-
-export default mongoose.model("Message", messageSchema);
+export default mongoose.model("Message", MessageSchema);
